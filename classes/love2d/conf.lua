0834fc9fb1d17298030ea3af265899a805d5f2d3
Gabriel-c0Nsp <gabrielsilvaaires62@gmail.com>
2023-09-27 12:37:28 -0300

    5º dia aprendendo Lua

---
+++ classes/love2d/conf.lua
@@ -0,0 +1,26 @@
+function love.conf(t)
+  -- t.identity = "data/saves"
+  -- t.version = "1.0"
+  -- t.console = false
+  -- t.externalstorage = true
+  -- t.gammacorrect = true
+  -- t.audio.mic = true
+
+  -- t.window.icon = "icon/game_icon.jpg"
+  t.window.title = "Cool Ball Game"
+  t.window.width = 1200
+  t.window.height = 700
+
+  -- t.window.minwidth = 500
+  -- t.window.minheight = 1000
+  -- t.window.resizable = true 
+
+  -- t.window.borderless = true 
+  -- t.window.vsync = 1
+  -- t.window.display = 1
+  -- t.window.fullscreen = true 
+  -- t.window.x = 100
+  -- t.window.y = 200
+
+  -- t.modules.timer = false
+end
---
+++ classes/love2d/main.lua
@@ -0,0 +1,13 @@
+_G.love = require("love") -- importing a module called love
+
+function love.load()
+  _G.number = 0
+end
+
+function love.update(dt)
+  number = number + 1
+end
+
+function love.draw()
+  love.graphics.print(number)
+end
