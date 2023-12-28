386511887f93a9fbd08c38f7a08d56f76719ec58
Gabriel-c0Nsp <gabrielsilvaaires62@gmail.com>
2023-12-27 23:21:22 -0300

    Criando um inimigo (8º dia aprendendo Lua)

--- save_the_ball_game/main.lua
+++ save_the_ball_game/main.lua
@@ -1,6 +1,10 @@
 local love = require("love")
+local enemy = require("Enemy")
+
+math.randomseed(os.time())
 
 local game = {
+	difficulty = 1,
 	state = {
 		menu = false,
 		pause = false,
@@ -15,19 +19,37 @@ local player = {
 	y = 30,
 }
 
+local enemies = {}
+
 function love.load()
 	love.window.setTitle("Save The Ball")
 	love.mouse.setVisible(false)
+
+	table.insert(enemies, 1, enemy())
 end
 
-function love.update(dt)
+function love.update()
 	player.x, player.y = love.mouse.getPosition()
+
+	for i = 1, #enemies do
+		enemies[1]:move(player.x, player.y)
+	end
 end
 
 function love.draw()
-	love.graphics.printf("FPS " .. love.timer.getFPS(), love.graphics.newFont(16), 10, love.graphics.getHeight() - 30, love.graphics.getWidth())
+	love.graphics.printf(
+		"FPS " .. love.timer.getFPS(),
+		love.graphics.newFont(16),
+		10,
+		love.graphics.getHeight() - 30,
+		love.graphics.getWidth()
+	)
 
 	if game.state["running"] then
+		for i = 1, #enemies do
+			enemies[1]:draw()
+		end
+
 		love.graphics.circle("fill", player.x, player.y, player.radius)
 	end
 
